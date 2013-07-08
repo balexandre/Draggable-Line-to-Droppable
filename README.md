ddb61ef46a2263c0b3a7ba3578ae14b6f0872b62
Bruno Alexandre <bma@gavekortet.dk>
2013-07-08 20:07:02 +0200

    updated to new version
    updated with latest versions of libraries;
    html refactored using JsRender;

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-This is the full source code from a StackOverflow Question that I made and answered myself.
+This is the **full source** code from a StackOverflow Question that I made and answered myself.
 
-A working demo can be found on JSBIN at [http://jsbin.com/evoloy/339/](http://jsbin.com/evoloy/339/edit)
+A working demo can be found on JSBIN at [http://jsbin.com/aliniz/2](http://jsbin.com/aliniz/2)
 
-The Question as well the Answer can be found on [StackOverflow](http://stackoverflow.com/questions/536676)
+The Question as well the Answer can be found on [StackOverflow](http://stackoverflow.com/questions/536676).
+
+***
+
+*versions:*
+
+**08 Jul 2013** <br> *v.1.3* - updated with latest versions of libraries; html refactored using JsRender;
+
+**03 Mar 2013** <br> *v.1.2* - Fixed links with working example as it was pointing to "latest" libraries;
+
+**29 Sep 2011** <br> *v.1.1* - Added GIT Repo; cleaned the code; update to latest framework versions;
+
+**11 Feb 2009** <br> *v.1.0* - First version from a need in a project;
--- scripts.js
+++ scripts.js
@@ -1,31 +1,42 @@
 /*
 **
 ** Part of StackOverflow Answer 
-**	http://stackoverflow.com/questions/536676/how-to-draw-a-line-between-draggable-and-droppable
+**  http://stackoverflow.com/questions/536676/how-to-draw-a-line-between-draggable-and-droppable
 **
 ** Created and Maintained by Bruno Alexandre (balexandre.com) 
 **
-** Last Edit: 29 September 2011
+** GIT REPO at https://github.com/balexandre/Draggable-Line-to-Droppable
+**
+** Last Edit: 08 July 2013
 **
 */
 
 var isDialogOpen = false,
-	myLines = [],
-	svg = null;
-	
+    myLines = [],
+    svg = null;
+    
 // icons
-var ico_userNormal  = 'icons/user-48x48.png',
-    ico_userChecked = 'icons/check-user-48x48.png';
+var ico_userNormal  = 'https://github.com/balexandre/Draggable-Line-to-Droppable/raw/master/icons/user-48x48.png',
+    ico_userChecked = 'https://github.com/balexandre/Draggable-Line-to-Droppable/raw/master/icons/check-user-48x48.png';
 
 $(document).ready(function () {
+  
+  // render persons
+  renderPersons();
 
+  // initialize & setup everything
+  initialize();
+
+});
+
+function initialize() {  
     // set up the drawing area from Body of the document
     //  -30px for the offset...
     $("#svgbasics")
         .css("height", $("#body").height() - 30)
         .css("width", $("#body").width() - 30);
 
-    // Mapping dialog box			
+    // Mapping dialog box           
     $("#dialogMappingResult").dialog({
         autoOpen: false,
         modal: true,
@@ -56,43 +67,42 @@ $(document).ready(function () {
 
                 // change class and image back to default
                 $("div .droppable")
-						.removeClass("ui-state-highlight")
-						.find("img")
-						.removeAttr("src")
-						.attr("src", ico_userNormal);
+                        .removeClass("ui-state-highlight")
+                        .find("img")
+                        .removeAttr("src")
+                        .attr("src", ico_userNormal);
 
-                // enable the droppable area	
+                // enable the droppable area    
                 $("div .droppable").droppable("enable");
 
                 // change class and image back to default
                 $("div .draggable")
-						.removeClass("ui-state-highlight")
-						.find("img")
-						.removeAttr("src")
-						.attr("src", ico_userNormal);
+                        .removeClass("ui-state-highlight")
+                        .find("img")
+                        .removeAttr("src")
+                        .attr("src", ico_userNormal);
                 // change icon back to default
                 $("div .draggable")
-						.find(".ui-icon-locked")
-						.removeClass("ui-icon-locked")
-						.addClass("ui-icon-shuffle");
+                        .find(".ui-icon-locked")
+                        .removeClass("ui-icon-locked")
+                        .addClass("ui-icon-shuffle");
 
                 // reset the draggable value
                 $("div .draggable")
-						.find("input:hidden")
-						.each(function () {
-						    $(this).val(
-								$(this).val().split("_")[0]
-							);
-						});
+                        .find("input:hidden")
+                        .each(function () {
+                           $(this).val( $(this).val().split("_")[0]
+                            );
+                        });
 
-                // enable the draggable area	
+                // enable the draggable area    
                 $("div .draggable").draggable("enable");
 
                 // reset the mapping dialog
                 $("#dialogMappingResult")
-						.find("ul")
-						.empty()
-						.append("<li>No mapping was done yet</li>");
+                        .find("ul")
+                        .empty()
+                        .append("<li>No mapping was done yet</li>");
 
                 $(this).dialog("close");
 
@@ -117,26 +127,26 @@ $(document).ready(function () {
         drop: function (event, ui) {
             // change class and image
             $(this)
-				.addClass("ui-state-highlight")
-				.find("img")
-				.removeAttr("src")
-				.attr("src", ico_userChecked);
+                .addClass("ui-state-highlight")
+                .find("img")
+                .removeAttr("src")
+                .attr("src", ico_userChecked);
 
-            // disable it so it can"t be used anymore		
+            // disable it so it can"t be used anymore       
             $(this).droppable("disable");
 
-            // change class and image of the source elemenet		
+            // change class and image of the source elemenet        
             $(ui.draggable)
-				.addClass("ui-state-highlight")
-				.find("img")
-				.removeAttr("src")
-				.attr("src", ico_userChecked);
+                .addClass("ui-state-highlight")
+                .find("img")
+                .removeAttr("src")
+                .attr("src", ico_userChecked);
 
-            // change the icon of the source element				
+            // change the icon of the source element                
             $(ui.draggable)
-				.find(".ui-icon-shuffle")
-				.removeClass("ui-icon-shuffle")
-				.addClass("ui-icon-locked");
+                .find(".ui-icon-shuffle")
+                .removeClass("ui-icon-shuffle")
+                .addClass("ui-icon-locked");
 
             var sourceValue = $(ui.draggable).find("input:hidden").val();
             var targetValue = $(this).find("input:hidden").val();
@@ -145,17 +155,17 @@ $(document).ready(function () {
             if ($("#dialogMappingResult").find("ul > li:first").html() == "No mapping was done yet")
                 $("#dialogMappingResult").find("ul").empty();
 
-            // append the mapping to the dialog	
+            // append the mapping to the dialog 
             $("#dialogMappingResult")
-				.find("ul")
-				.append("<li>" + sourceValue + " >> " + targetValue + "</li>");
+                .find("ul")
+                .append("<li>" + sourceValue + " >> " + targetValue + "</li>");
         
             // change the input element to contain the mapping target and source
             $(ui.draggable)
-				.find("input:hidden")
-				.val(sourceValue + "_" + targetValue);
+                .find("input:hidden")
+                .val(sourceValue + "_" + targetValue);
 
-            // disable it so it can"t be used anymore	
+            // disable it so it can"t be used anymore   
             $(ui.draggable).draggable("disable");
 
             svgDrawLine($(this), $(ui.draggable));
@@ -171,8 +181,7 @@ $(document).ready(function () {
     });
 
     svg = Raphael("svgbasics", $("#svgbasics").width(), $("#svgbasics").height());
-
-});
+}
 
 function svgClear() {
     svg.clear();
@@ -197,27 +206,27 @@ function svgDrawLine(eTarget, eSource) {
         var color = colours[random(9)];
 
         // draw lines
-        // http://raphaeljs.com/reference.html#path			
+        // http://raphaeljs.com/reference.html#path         
         var a = "M" + originX + " " + originY + " L" + (originX + space) + " " + originY; // beginning
         var b = "M" + (originX + space) + " " + originY + " L" + (endingX - space) + " " + endingY; // diagonal line
         var c = "M" + (endingX - space) + " " + endingY + " L" + endingX + " " + endingY; // ending
         var all = a + " " + b + " " + c;
 
-        /*
-        // log (to show in FF (with FireBug), Chrome and Safari)			
+        /**/
+        // log (to show in FF (with FireBug), Chrome and Safari)            
         console.log("New Line ----------------------------");
-        console.log("originX: " + originX + " | originY: " + originY + " | endingX: " + endingX + " | endingY: " + endingY + " | space: " + space + " | color: " + color );				
+        console.log("originX: " + originX + " | originY: " + originY + " | endingX: " + endingX + " | endingY: " + endingY + " | space: " + space + " | color: " + color );             
         console.log(all); 
-        */
+        /**/
 
         // write line
         myLines[myLines.length] = svg
-										.path(all)
-										.attr({
-										    "stroke": color,
-										    "stroke-width": 2,
-										    "stroke-dasharray": "--."
-										});
+                .path(all)
+                .attr({
+                   "stroke": color,
+                   "stroke-width": 2,
+                   "stroke-dasharray": "--."
+                });
 
     }, 1000);
 
@@ -227,5 +236,31 @@ function random(range) {
     return Math.floor(Math.random() * range);
 }
 
+// Render persons on both sides using JsRender
+function renderPersons() {
+  console.log('redering left persons...');
+  var htmlL = $("#personTemplate").render(leftPersons);
+  $("#leftPanel").html(htmlL);
+  
+  console.log('redering right persons...');
+  var htmlR = $("#personTemplate").render(rightPersons);
+  $("#rightPanel").html(htmlR);
+  
+  // give all the correct image
+  $(".person img").each(function() { 
+    $(this).attr("src", ico_userNormal); });
+}
+
 // random colors are not that random after all
 var colours = ["purple", "red", "orange", "yellow", "lime", "green", "blue", "navy", "black"];
+
+// list of persons to render
+var leftPersons = [
+  {id:1, name:'Peter Fagerholm', position:'Developer', cls:'draggable'},
+  {id:2, name:'Kjell Öhlén', position:'Manager', cls:'draggable'}
+];
+var rightPersons = [
+  {id:3, name:'Elsa Hallberg', position:'C.E.O.', cls:'droppable'},
+  {id:4, name:'Marie Berglund', position:'Recepcionist', cls:'droppable'},
+  {id:5, name:'Kjell Öhlén', position:'Manager', cls:'droppable'}
+];
